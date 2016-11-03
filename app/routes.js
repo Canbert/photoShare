fe870a4707311955481da31ca7e43fe3f217e85d
canbert <thompus987@gmail.com>
2016-11-03 14:50:40 +0000

    Updated routes,package, server

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,11 @@
-/**
- * Created by scott on 03/11/16.
- */
+
+
+module.exports = function (app) {
+    app.get('*', function (req, res) {
+        res.sendFile('./public/index.html');
+    });
+
+    app.get('/login', function (req, res) {
+        res.sendFile('./public/login/index.html');
+    });
+};
