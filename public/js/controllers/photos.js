5d88c8f85a5597f19a4137bf87de605a438fdfac
Canbert <thompus987@gmail.com>
2016-11-19 17:08:18 +0000

    added create photo to photo controller

--- public/js/controllers/photos.js
+++ public/js/controllers/photos.js
@@ -26,4 +26,25 @@ angular.module('photoController', [])
                 $scope.loading = false;
             });
 
+        // CREATE ==================================================================
+        // when submitting the add form, send the text to the node API
+        $scope.createPhoto = function() {
+
+            // validate the formData to make sure that something is there
+            // if form is empty, nothing will happen
+            if ($scope.formData.text != undefined) {
+                $scope.loading = true;
+
+                // call the create function from our service (returns a promise object)
+                Photos.create($scope.formData)
+
+                // if successful creation, call our get function to get all the new todos
+                    .success(function(data) {
+                        $scope.loading = false;
+                        $scope.formData = {}; // clear the form so our user is ready to enter another
+                        $scope.photos = data; // assign our new list of todos
+                    });
+            }
+        };
+
     }]);
