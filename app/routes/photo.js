3518b07b55876422d6ea0f814ad3db341769eab2
canbert <thompus987@gmail.com>
2016-11-25 23:19:46 +0000

    Photo render parameter

--- app/routes/photo.js
+++ app/routes/photo.js
@@ -6,15 +6,19 @@ module.exports = function (app) {
     // =====================================
     app.get('/photo/:photo_id', function (req, res) {
 
-        Photo.findById(req.params.photo_id, function (err, photo) {
-            if(err)
-                res.send(err);
-            else
-                console.log("hello");
+        console.log(req.params.photo_id);
 
-                res.render('pages/photo', {
-                    photo: photo
-                });
+        Photo.findById(req.params.photo_id)
+            .populate('user','username')
+            .exec(function (err, photo) {
+                if(err)
+                    console.log(err);
+                if(photo)
+                    res.render('pages/photo', {
+                        photo: photo
+                    });
+                else
+                    res.redirect("/");
         });
 
     });
