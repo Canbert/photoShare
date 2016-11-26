24977834cf8d58d421605c592f7110d61c4aa913
Canbert <thompus987@gmail.com>
2016-11-26 14:54:41 +0000

    Added user connect and disconnect to users array

--- app/chat.js
+++ app/chat.js
@@ -1,9 +1,22 @@
 
 module.exports = function (io) {
 
+    var users = [];
+
     io.on('connection', function (socket) {
         console.log('Socket.io chat: user connected');
         io.emit('serverMsg', 'User connected');
+
+        socket.on('newUser',function (data, callback) {
+            if (users.indexOf(data) != -1){
+                callback(false);
+            } else {
+                callback(true);
+                socket.user = data;
+                users.push(socket.user);
+                io.sockets.emit('users', users);
+            }
+        });
 
         socket.on('userMsg', function (msg) {
             console.log("Socket.io chat: " + msg.username + ' said ' + msg.message);
@@ -13,6 +26,9 @@ module.exports = function (io) {
         socket.on('disconnect', function () {
             console.log('Socket.io chat: user disconnected');
             io.emit('serverMsg', 'User disconnected');
+            if(!socket.user) return;
+            users.splice(users.indexOf(socket.user),1);
+            io.sockets.emit('users', users);
         });
     });
 }
