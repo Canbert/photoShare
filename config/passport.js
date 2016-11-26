d5e69aa5c6757faa8ca095fbbe3eeaa3d0837b9d
Scott Thomson <1508551@rgu.ac.uk>
2016-11-26 19:48:47 +0000

    Set session password to null on login (#32)

--- config/passport.js
+++ config/passport.js
@@ -117,6 +117,9 @@ module.exports = function(passport) {
                 if (!user.validPassword(password))
                     return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
 
+                if(user)
+                    user.password = null;
+
                 // all is well, return successful user
                 return done(null, user);
             });
