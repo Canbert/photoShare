6a6c4c43d1651f1f2af45cc3e0dff958a6916241
canbert <thompus987@gmail.com>
2016-12-01 17:19:36 +0000

    Added tagsService

---
+++ public/js/services/tagsService.js
@@ -0,0 +1,23 @@
+angular.module('tagsService', [])
+
+    // super simple service
+    // each function returns a promise object
+    .factory('Tags', ['$http',function($http) {
+        return {
+            get : function() {
+                return $http.get('/api/tags');
+            },
+            get : function (id) {
+                return $http.get('/api/tags/' + id);
+            },
+            update: function (id, tagData) {
+                return $http.patch('/api/tags' + id, tagData);
+            },
+            create : function(tagData) {
+                return $http.post('/api/tags', tagData);
+            },
+            delete : function(id) {
+                return $http.delete('/api/tags/' + id);
+            }
+        }
+    }]);
