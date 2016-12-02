fd87aacd6f92e274b4fd340464d069539539e2d3
Canbert <thompus987@gmail.com>
2016-12-02 13:55:25 +0000

    Removed get users

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,7 +7,6 @@ module.exports = function (app) {
     // =====================================
     app.get('/admin',isLoggedIn, function (req, res) {
         if(req.user.privilege == 2){
-            getUsers();
             res.render('pages/admin', {
                 user : req.user // get the user out of session and pass to template
             });
@@ -27,15 +26,5 @@ function isLoggedIn(req, res, next) {
 
     // if they aren't redirect them to the home page
     res.redirect('/');
-}
-
-function getUsers() {
-    // get all the users
-    User.find({}, function(err, users) {
-        if (err) throw err;
-
-        // object of all the users
-        console.log(users);
-    });
 }
 
