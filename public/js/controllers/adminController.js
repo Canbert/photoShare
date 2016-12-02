5aca8397540f8d77905f1510f87468d5f1b457a8
Canbert <thompus987@gmail.com>
2016-12-02 14:05:04 +0000

    Update adminController to get users

--- public/js/controllers/adminController.js
+++ public/js/controllers/adminController.js
@@ -1,15 +1,16 @@
 angular.module('adminController', [])
 
     // inject the  Admin service factory into our controller
-    .controller('mainController', ['$scope','$http','Users', function($scope, $http, Users) {
-        $scope.formData = {};
-        $scope.loading = true;
+    .controller('adminCtrl', ['$scope','$http','Users', function($scope, $http, Users) {
+
+        $scope.users;
 
         // GET =====================================================================
         // when landing on the page, get all users and show them
         // use the service to get all the users
         Users.get()
             .success(function(data) {
-                $scope.user = data;
+                $scope.users = data;
             });
+
     }]);
